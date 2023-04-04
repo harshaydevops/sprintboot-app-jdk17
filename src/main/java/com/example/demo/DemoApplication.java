80a76263914e5a929c3870accf728daf10d3161d
Ubuntu <ubuntu@ip-172-31-2-55.us-east-2.compute.internal>
2023-04-04 22:36:02 +0000

    initial draft

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+    @GetMapping("/")
+    @ResponseBody
+    public String currentUserName() {
+        return "Hello World!";
+    }	
+}
